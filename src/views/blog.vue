fed0702a1fcf8b2c5c4440a56ea5b42ccaa899b3
casparsylwan <casparsylwan@gmail.com>
2020-05-24 00:59:33 +0200

    blog

--- src/views/blog.vue
+++ src/views/blog.vue
@@ -1,25 +1,148 @@
 <template>
   <div class="blog">
-    blog
+    <div class="box">
+      <div class="top">
+          <div class="arrow-back">
+            <p>
+              &#8592; 
+            </p>
+          </div>
+        </div>
+      <div class="left">
+      
+      <Book class="book" v-bind:info="info" ></Book>
+      </div>
+      <div class="rigth">
+        <h1>{{ info.title }}</h1>
+        <p> by {{ info.author }} </p>
+        <section>
+          {{ info.plot }}
+          <div class="info">
+            <p> pages: {{ info.pages }}  </p>
+            <p> audience: {{ info.audience }} </p>
+            <p> publisher: {{ info.publisher }} </p>
+            <p> year: {{ info.year }} </p>
+          </div>
+          <div class="btn">
+            <p>Lend book</p>
+            
+          </div>
+        </section>
+      </div>
+    </div>
     {{ $route.params.id }}
-    <book></book>
+    
   </div>
 </template>
 
 <script>
-import book from '../components/book';
+import Book from '../components/book';
+import json from '../assets/data.json';
 
 
 export default {
   name: 'blog',
   components: {
-    book
+    Book   
+  },
+    data(){
+        return{
+            info: json.books[this.$route.params.id-1],
+            bookInfo:json.books
+            
+              }
+          },
+           methods:{
+              setParam(){
 
-    
-  }
+                  let index = this.bookInfo.findIndex(x => x.id == this.$route.params.id);
+                   
+                  return index;
+              }
+          }
 
 }
 </script>
 <style scoped lang="scss">
+
+  .box{
+    padding: 2rem;
+    background-color: #000;
+    display: grid;
+    grid-template-areas: 
+    "top top top top"
+    "book book text text";
+   
+    color: #fff;
+
+    .top{
+      grid-area: top;
+      width: 100;
+    }
+
+    .arrow-back{
+      
+      height: 2.5rem;
+      width: 2.5rem;
+      border-radius: 100%;
+      background-color: #777;
+      color: #000;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-size: 1.5rem;
+      font-weight: bold;
+      margin-bottom: 2rem;
+
+        p{
+          margin-top: -.25rem;
+        }
+    }
+
+    .book{
+      grid-area: book;
+    }
+
+    
+
+    .rigth{
+      grid-area: text;
+      margin-left: 2rem;
+
+      h1{
+        font-weight: bolder;
+        font-size: 2rem;
+      }
+
+      .info{
+        background-color: lighten($color: #000000, $amount: 10%);
+        padding: 1rem;
+        display: grid;
+        grid-template-columns: repeat(2,1fr);
+      }
+
+      .btn{
+        height: 2rem;
+        width: 30%;
+        border-radius: 10px;
+        background-color: #fff;
+        color:#000;
+        padding: 1%;
+        text-align: center;
+
+        p{
+          font-size: 1.5rem;
+        }
+          
+        
+
+       
+      }
+
+    }
+
+  }
+
+  
 
 </style>
